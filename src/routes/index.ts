0f203bb68ffbaa9b8a4df5830c96d9fbfca4398f
ElCaffeesito <63433977+ElCaffeesito@users.noreply.github.com>
2024-11-26 21:31:42 -0600

    Charizard

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import welcomeRoutes from './welcome';
 import registerRoutes from './register';
 import loginRoutes from './login';
 import homeRoutes from './home';
@@ -37,7 +36,6 @@ router.post('/logout', logout, (req, res) => {
     res.send("You've logged out successfuly")
 })
 
-router.use('/welcome', welcomeRoutes);
 router.use('/register', registerRoutes);
 router.use('/login', loginRoutes);
 router.use('/home', homeRoutes);
